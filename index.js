9ec0d78f57e5ed0ca656344e44fc5ef7a5d9afc8
Mohammed Saayim <mdsaayim@gmail.com>
2020-12-21 07:14:47 +0000

    style- add chalk

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var readlineSync=require('readline-sync');
+const chalk = require('chalk');
+
 var userName=prompt("What is your name?");
 
-console.log('Welcome '+userName);
+console.log(chalk.bgGreenBright('Welcome '+userName));
 var year=prompt("What is your year of birth?");
 
 if(isNaN(year)){
   console.log("Error: Entered value is invalid, please enter your birth year in number format(e.g: 1999)")
-  
 }
 else{
 if((year%4==0&&year%100!=0)||(year%4==0&&year%100==0&&year%400==0)){
-  console.log("You were born in a leap year")
-  console.log("Share this on social media")
+  console.log(chalk.bgCyan("You were born in a leap year"))
+  console.log(chalk.bgBlackBright("Share this on social media"))
 }
 else{
-  console.log("You were born in a non-leap year")
+  console.log(chalk.bgCyan("You were born in a non-leap year"))
 }
 }
